b8338d159059af401395077e5f80f1d4de992b44
Mr. Crazy Unknown <suchiv2311@gmail.com>
2020-09-15 22:21:55 +0530

    Improved implementation

--- keyboard_twist_teleop/scripts/teleop_drone.py
+++ keyboard_twist_teleop/scripts/teleop_drone.py
@@ -5,16 +5,6 @@ import curses
 import rospy
 from geometry_msgs.msg import Twist
 
-flags = {}
-flags[KeyCode(char = 'w')] = 0
-flags[KeyCode(char = 'a')] = 0
-flags[KeyCode(char = 's')] = 0
-flags[KeyCode(char = 'd')] = 0
-flags[KeyCode(char = 'q')] = 0
-flags[KeyCode(char = 'e')] = 0
-flags[Key.shift] = 0
-flags[Key.space] = 0
-
 
 class Main:
     def __init__(self):
@@ -33,6 +23,15 @@ class Main:
         self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
         rospy.init_node('teleop', anonymous=True)
         self.msg = Twist()
+        self.flags = {}
+        self.flags[KeyCode(char = 'w')] = 0
+        self.flags[KeyCode(char = 'a')] = 0
+        self.flags[KeyCode(char = 's')] = 0
+        self.flags[KeyCode(char = 'd')] = 0
+        self.flags[KeyCode(char = 'q')] = 0
+        self.flags[KeyCode(char = 'e')] = 0
+        self.flags[Key.shift] = 0
+        self.flags[Key.space] = 0
         self.screen = curses.initscr()
         self.screen_clear()
         self.screen.refresh()
@@ -76,8 +75,8 @@ class Main:
         try:
             if rospy.is_shutdown():
                 exit(0)
-            if key in flags:
-                flags[key] = 1
+            if key in self.flags:
+                self.flags[key] = 1
             self.move()
         except rospy.ROSInterruptException as e:
             print(e)
@@ -86,8 +85,8 @@ class Main:
         try:
             if rospy.is_shutdown():
                 exit(0)
-            if key in flags:
-                flags[key] = 0
+            if key in self.flags:
+                self.flags[key] = 0
             elif key == KeyCode(char = 't'):
                 self.change_speed(True)
             elif key == KeyCode(char = 'g'):
@@ -97,6 +96,7 @@ class Main:
             elif key == KeyCode(char = 'h'):
                 self.change_rot(False)
             if key == Key.esc:
+                self.flags = dict.fromkeys(self.flags.iterkeys(), 0)
                 curses.nocbreak()
                 self.screen.keypad(False)
                 self.screen_clear()
@@ -111,10 +111,10 @@ class Main:
     def move(self):
         if(self.screen.getmaxyx()[0] <= self.cmd_count):
             self.screen_clear()
-        self.msg.linear.x = (flags[KeyCode(char = 'w')] - flags[KeyCode(char = 's')]) * self.speed
-        self.msg.linear.y = (flags[KeyCode(char = 'a')] - flags[KeyCode(char = 'd')]) * self.speed
-        self.msg.linear.z = (flags[Key.space] - flags[Key.shift]) * self.speed
-        self.msg.angular.z = (flags[KeyCode(char = 'q')] - flags[KeyCode(char = 'e')]) * self.rot
+        self.msg.linear.x = (self.flags[KeyCode(char = 'w')] - self.flags[KeyCode(char = 's')]) * self.speed
+        self.msg.linear.y = (self.flags[KeyCode(char = 'a')] - self.flags[KeyCode(char = 'd')]) * self.speed
+        self.msg.linear.z = (self.flags[Key.space] - self.flags[Key.shift]) * self.speed
+        self.msg.angular.z = (self.flags[KeyCode(char = 'q')] - self.flags[KeyCode(char = 'e')]) * self.rot
         self.pub.publish(self.msg)
         text = "linear x:" + str(round(self.msg.linear.x, 2))
         text += ", y:" + str(round(self.msg.linear.y, 2))
